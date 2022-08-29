306948d1b508e118f3cfb4165f407b0550a00953
Dario <dario.rosa@evertecinc.com>
2022-08-29 14:30:38 -0300

    primera version del id3

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
+#import seaborn as sns
 import numpy as np
 import warnings
 from sklearn.model_selection import train_test_split
@@ -69,6 +69,8 @@ Fase 1
 	- Todos los valores a reales
 	- Enumerados: crear columnas binarias
 	- Normalizar los valores
+        - los atributos continuos, cambiarlos como se vio en el teoricos, diapo arboles 33
+                
 	
 Fase 2
 	- Separar conjuntos Entrenamiento, Validacion, Test
@@ -76,17 +78,78 @@ Fase 2
 	
 """
 
-"""
-ID3:
-Crear una raíz
-• Si todos los ej. tienen el mismo valor → etiquetar con ese valor
-• Si no me quedan atributos → etiquetar con el valor más común
-• En caso contrario:
-    ‣ La raíz pregunta por A, atributo que mejor clasifica los ejemplos
-    ‣ Para cada valor vi de A
-        ๏ Genero una rama 
-        ๏ Ejemplosvi={ejemplos en los cuales A=vi }
-        ๏ Si Ejemplosvi es vacío → etiquetar con el valor más probable
-        ๏ En caso contrario → ID3(Ejemplosvi, Atributos -{A})
+class G02Tree():
+        def __init__(self, idColumn):
+                self.IdColumn = idColumn
+                self.Nodes= []
 
-"""
+class G02TreeSheet(G02Tree):
+        def __init__(self, idColumn, returnValue):
+                super().__init__(idColumn)
+                self.ReturnValue= returnValue
+
+class G02TreeNode():
+        #valor, arboles o un valor de retorno
+        def __init__(self, value):
+                self.Value = value
+                self.SubTree = None                
+
+
+def GetBestAtt(df): #retornar el mejor atributo para separara
+        return df.columns[0] #por ahora retornamos el primer atributo
+        #TODO usar algun algoritmo del teorico, entropia?
+
+
+dfGlobal = df.copy()
+def ID3_DecisionTree(pdf):
+        df = pdf.copy()
+        #Elegir un atributo
+        idColumn = GetBestAtt(df) # o Afuera de la funcion?
+        #Crear una raíz
+        ret = G02Tree(idColumn)
+
+        #• Si todos los ej. tienen el mismo valor → etiquetar con ese valor
+        uniqueStrokes = df.stroke.unique()
+        if(uniqueStrokes.size == 0):
+                return G02TreeSheet(idColumn, uniqueStrokes[0])
+        
+        #• Si no me quedan atributos → etiquetar con el valor más común
+        if(df.columns.size == 1):
+                return G02TreeSheet(idColumn, df.stroke.mode())
+
+        #• En caso contrario:
+        #    ‣ La raíz pregunta por A, atributo que mejor clasifica los ejemplos
+
+        #    ‣ Para cada valor vi de A 
+        #TODO ver que hacer con los atributos continuos, podriamos ver de  crear un array con todos los valores posibles para una columna
+        for vi in dfGlobal[idColumn].value_counts() : #dfGlobal el df original
+                #๏ Genero una rama
+                node = G02TreeNode(vi)
+                #๏ Ejemplosvi={ejemplos en los cuales A=vi }
+                ejemplosvi = df[df[idColumn] == vi]
+                #๏ Si Ejemplosvi es vacío → etiquetar con el valor más probable
+                if(len(ejemplosvi) == 0):
+                        node.SubTree = G02TreeSheet(idColumn, df.stroke.mode())
+                else: #En caso contrario → ID3(Ejemplosvi, Atributos -{A})
+                        #Atributos -{A}
+                        del ejemplosvi[idColumn]
+                        node.SubTree = ID3_DecisionTree(ejemplosvi)
+                ret.Nodes.append(node)
+        
+        return ret
+                
+
+
+              
+
+
+        
+         
+        #        
+        #        
+        #        ๏ En caso contrario → ID3(Ejemplosvi, Atributos -{A})
+        #         
+        return ret
+
+
+ID3_DecisionTree(df)
